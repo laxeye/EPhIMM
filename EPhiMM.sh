06a43b3f5187ffe0158baef2eeda0c922cd03385
Aleksei Korzhenkov <oscypek@ya.ru>
2019-08-16 15:52:06 +0300

    Initial commit

---
+++ EPhiMM.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+
+#Get a path to script
+EPHIMMPATH=$(dirname $(readlink -f $0))
+#Defaults
+MRKDIR="$EPHIMMPATH/markers/"
+HMMEVALUE=1e-6
+#Set maximum gap fraction in column to leave it in alignment
+MAXGAPFRACTION=50
+OUTPUTFOLDER="output-$(date +%F)"
+PREDICT=0
+
+#Parse args
+while (( "$#" )); do
+  case "$1" in
+    -m|--markers)
+      if [[ -d $2 ]]; then
+        MRKDIR=$2
+      fi
+      shift 2
+      ;;
+    -g|--max-gap-fraction)
+      if [[ $2 -gt 0 && $2 -lt 100 ]]; then
+        MAXGAPFRACTION=$2
+      else
+        echo "Errror: Max gap fraction should be in (0,100)!"
+        exit 1
+      fi
+      shift 2
+      ;;
+    -e|--evalue)
+      HMMEVALUE=$2
+      shift 2
+      ;;
+    -o|--output)
+      OUTPUTFOLDER=$2
+      shift 2
+      ;;
+    -p|--predict-proteins)
+      PREDICT=1
+      shift 1
+      ;;
+    -h|--help)
+      echo -e "\nEPhIMM v 0.1 - Express Phylogenetic Inference based on Multiple Markers\n"
+      echo -n "Usage: $0 [-p] [-e|--evalue <N>] [-g|--max-gap-fraction <N>] [-o|--output <directory>] "
+      echo -e "[-m|--markers <directory>] [-h|--help]\n"
+      exit 0
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      echo "Possible flags: -m, -e, -g, -o, -p " >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+
+#Using half of available CPU threads for hmmsearch
+threads=`grep -c "^processor" /proc/cpuinfo`
+if [[ $threads -gt 1 ]]; then
+    (( threads/=2 ))
+else
+    threads=1
+fi
+
+if [[ -d $OUTPUTFOLDER ]]; then
+    rm -r $OUTPUTFOLDER
+fi
+mkdir -p $OUTPUTFOLDER
+logfile="$OUTPUTFOLDER/fetching.log"
+
+echo "#Analysys started at $(date)" | tee $logfile
+
+# Predict CDS
+if [[ $PREDICT -eq 1 ]]; then
+    echo -e "#$(date +"%T")\tProtein prediction started" | tee -a $logfile
+    for i in *fna; do prodigal -o /dev/null -i $file -a ${i/fna/faa} -q; done
+fi
+
+# Indexing fasta files
+echo -e "#$(date +"%T")\tIndexing files" | tee -a $logfile
+for file in *faa; do esl-sfetch --index $file > /dev/null; done
+
+echo "#Directory with HMM profiles of marker genes: $MRKDIR" | tee -a $logfile
+
+markercount=`ls $MRKDIR/*hmm | wc -l`
+genomecount=`ls *faa | wc -l`
+
+if [[ $markercount -eq 0 ]]; then
+    echo "No HMM profiles found in $MRKDIR" | tee -a $logfile
+else
+    echo "#$markercount HMM profiles found" | tee -a $logfile
+fi
+
+echo -e "#$(date +"%T")\tSearching and extracting marker genes" | tee -a $logfile
+
+for marker in $(ls $MRKDIR/);
+    do if [[ -d $marker ]]; then
+        rm -r -f $marker
+    fi
+    mkdir $marker;
+    mrkpath=$MRKDIR$marker
+
+    for file in *faa;
+    do hmmsearch --cpu $threads -E $HMMEVALUE -o /dev/null --tblout /dev/stdout $mrkpath $file | grep -v "^#" > $marker/${file/faa/out};
+
+    lines=`wc -l < $marker/${file/faa/out}`
+
+    case $lines in
+        0)
+            echo -e "Marker gene \"$marker\" not found in $file!" | tee -a $logfile; 
+    		echo -e ">S2.${file/.faa/}\nXXXXXX" > $marker/$file;
+    		;;
+        1)
+            esl-sfetch -o $marker/$file -n $marker.${file/.faa/} $file $(awk '{print $1}' $marker/${file/faa/out}) > /dev/null;
+            ;;
+        *)
+            echo -e "Marker gene \"$marker\" has many copies in $file! You should inspect sequences manually!" | tee -a $logfile;
+            let n=1
+            for line in $(awk '{print $1}' $marker/${file/faa/out})
+                do esl-sfetch -n $marker.${file/.faa/}.$n $file $line >> $marker/$file;
+                ((n++));
+            done;
+            ;;
+    esac
+
+    done;
+done;
+
+echo -e "#$(date +"%T")\tCollecting statistics" | tee -a $logfile
+
+echo -e "#Genome\tMissing markers\tMultiple copies" > $OUTPUTFOLDER/genomes.stats.txt
+for i in *faa;
+    do missing=`grep $i $logfile | grep -c "not found"`;
+    multi=`grep $i $logfile | grep -c "many copies"`;
+    echo -e "$i\t$missing\t$multi" >> $OUTPUTFOLDER/genomes.stats.txt;
+done;
+
+echo "#Genomes with more than 50% missing marker genes:" > $OUTPUTFOLDER/bad.genomes.txt
+grep -v "^#" genomes.stats.txt | awk '$2 > "'"$markercount"'"/2' >> $OUTPUTFOLDER/bad.genomes.txt
+echo "#Genomes with more than 20% multi-copy marker genes:" >> $OUTPUTFOLDER/bad.genomes.txt
+grep -v "^#" genomes.stats.txt | awk '$3 > "'"$markercount"'"/5' >> $OUTPUTFOLDER/bad.genomes.txt
+
+echo -e "#Marker\tMissing markers\tMultiple copies" > hmm.stats.txt
+for marker in $(ls $MRKDIR/);
+    do missing=`grep "$marker" $logfile | grep -c "not found"`
+    multi=`grep "$marker" $logfile | grep -c "many"`
+    echo -e "$marker\t$missing\t$multi" >> $OUTPUTFOLDER/hmm.stats.txt;
+done
+
+echo "#Markers missing in more than 50% genomes:" > $OUTPUTFOLDER/bad.hmms.txt
+grep -v "^#" hmm.stats.txt | awk '$2 > "'"$genomecount"'"/2' >> $OUTPUTFOLDER/bad.hmms.txt
+echo "#Markers with multiple copies in more than 20% genomes:" >> $OUTPUTFOLDER/bad.hmms.txt
+grep -v "^#" hmm.stats.txt | awk '$3 > "'"$genomecount"'"/5' >> $OUTPUTFOLDER/bad.hmms.txt
+
+for file in *faa;
+do 
+    if ! grep -q "$file" $OUTPUTFOLDER/bad.genomes.txt ; then
+        echo ">${file/.faa/}" > $OUTPUTFOLDER/$file;
+        cat *hmm/$file | sed 's/\*//' | grep -v ">" | grep -v "^X\+$"  >> $OUTPUTFOLDER/$file;
+    else
+        echo "Genome $file will not be used because of incompleteness or contamination."
+    fi
+done
+
+cat $OUTPUTFOLDER/*faa > $OUTPUTFOLDER/all.markers.faa
+
+echo "Concatenated markers stored in \"$OUTPUTFOLDER/all.markers.faa\""
+
+echo -e "#$(date +"%T")\tPerforming MSA" | tee -a $logfile
+
+mafft --auto  $OUTPUTFOLDER/all.markers.faa | sed 's/X/-/g' > $OUTPUTFOLDER/all.aligned.faa
+
+echo "It's strictly recommended to check alignment file \"$OUTPUTFOLDER/all.aligned.faa\""
+echo "You may automaticly delete all columns with more than 50% gaps"
+
+read -p "Press \"N\" if You like to edit file manually.
+Press \"Y\" if You like to clean gaps automaticly: " response
+
+if [[ $response -eq "Y" ]]; then
+    alnfilename="$OUTPUTFOLDER/all.aligned.e.faa"
+    java -jar $EPHIMMPATH/aleditor.jar $OUTPUTFOLDER/all.aligned.faa $MAXGAPFRACTION > $alnfilename
+    echo -e "#$(date +"%T")\tPerforming autoedit of MSA" | tee -a $logfile
+else
+    read -p "Provide a filename of edited alignment:" alnfilename
+fi
+
+echo -e "#$(date +"%T")\tPerforming phylogenetic inference" | tee -a $logfile
+
+fasttree -gamma -lg $alnfilename > $OUTPUTFOLDER/all.markers.nwk
+
+#Low number of genomes and markers:
+#raxmlHPC -f a -x 2019 -N 100 -m PROTCATLG -n markers -s alnfilename -p 2019 -T $threads
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # EPhIMM
-Express Phylogenetic Inference based on Multiple Markers
+**E**xpress **Ph**ylogenetic **I**nference based on **M**ultiple **M**arkers
+
+**EPhIMM** is a shell script for express phylogenetic analysis of prokaryotic genomes.
+
+
+## Dependencies
+
+* **esl-sfetch** from Easel for <https://github.com/EddyRivasLab/easel>
+* **mafft** <https://mafft.cbrc.jp/alignment/software/> for multiple sequence alignment
+* **fasttree** <http://www.microbesonline.org/fasttree/> for tree inference
+* **Java 8** for autoedit of alignment (typically installed in Linux ditributions)
+
+Install some of them with `conda install mafft fasttree easel`
+
+If You start from nucleotide genomic sequences You will need **prodigal** <https://github.com/hyattpd/Prodigal>. It could be installed with *conda* or Your linux distribution's package manager like *apt*, *yum* etc.
+
+
+## How it works
+
+Extract HMM profiles for marker genes from `markers.tar.gz` using `tar xzf markers.tar.gz`.
+
+Run EPhIMM.sh in directory containing files with protein sequences. E.g. if You downloaded EPhiMM to `/home/user/software/EPhiMM/` and data located in `/home/user/data/mygenomes`:
+
+`user@pc:~/ cd /home/user/data/mygenomes`
+`user@pc:/home/user/data/mygenomes$ /home/user/software/EPhiMM/EPhiMM.sh`
+
+Default output folder is `output-DATE`, where DATE is current date. You can provide different output folder using flag **-o**, e.g. `EPhiMM.sh -o MyOutput`.
+
+If You work with nucleotide genomic sequences use flag **-p** to predict CDS with Prodigal.
+
+After marker genes search. fetching and alignment You will be asked for manual or automatic alignment editing. In case of automatic columns containing more than 50% gaps will be removed from the alignment using java tool *aleditor.jar*. In case of manual editing open file `all.aligned.faa` in the output folder, make changes and save it. You will be prompted for relative path to edited file, e.g. if You saved file as edited.version.faa in final directory type `final/edited.version.faa`.
+
+You can check `genomes.stats.txt` and `hmm.stats.txt` for missing and duplicated genes. Resulting alignment and tree will be stored in `final/` directory.
+
+
+## Markers
+
+Marker genes should be provided as HMM profiles.
+
+Initial set containing 41 single copy marker genes was taken from CheckM software (Parks et al., 2015), 2 markers from the original set(TIGR00344 and TIGR00422) were excluded due to multiple hits.
+
+If You like to use Your own set of markers put `.hmm` files in a directory, e.g. /home/user/MyMarkers, and run the tool providing path to the markers with flag **-m**: `EPhIMM.sh -m /home/user/MyMarkers/`.
+
+Default e-value for hmmsearch is 1e-6, You can set it with glag **-e**: e.g. `EPhIMM.sh -e 1e-10`.
+
+
+## References
+
+Parks, D. H., Imelfort, M., Skennerton, C. T., Hugenholtz, P., & Tyson, G. W. (2015). CheckM: assessing the quality of microbial genomes recovered from isolates, single cells, and metagenomes. Genome research, 25(7), 1043-1055.
+
+Katoh, K., & Standley, D. M. (2013). MAFFT multiple sequence alignment software version 7: improvements in performance and usability. Molecular biology and evolution, 30(4), 772-780.
+
+Price, M.N., Dehal, P.S., and Arkin, A.P. (2010) FastTree 2 -- Approximately Maximum-Likelihood Trees for Large Alignments. PLoS ONE, 5(3):e9490. doi:10.1371/journal.pone.0009490. 
